1434673f8f8dc53bce5c350f04ac8df67b2ff84f
garciadeblas <gerardo.garciadeblas@telefonica.com>
2020-05-07 15:05:58 +0000

    Added files for deb package creation
    Change-Id: I7cd40b9696009ffd778ba69ec2e6723a7f8e32e4
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- devops-stages/stage-build.sh
+++ devops-stages/stage-build.sh
@@ -12,4 +12,5 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo "BUILD"
+./build-debpkg.sh
+echo "BUILD DONE"
--- devops-stages/stage-test.sh
+++ devops-stages/stage-test.sh
@@ -11,4 +11,4 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo "TEST"
+echo "UNITTEST DONE"
